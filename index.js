f4b9c1aa3833191852dccbacc132346f8fb85a5a
Lisha Kuinkel <ltkuinkel@gmail.com>
2023-09-02 13:01:00 -0600

    added some comments to index.js file

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+// Import dependencies
 const express = require('express');
 const db = require('./ config/connection');
 const routes = require('./routes');
 
+// Setup enviroment variables
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Use middleware to parse incoming data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Use routes defined in routes.js
 app.use(routes);
 
+// Connect to MongoDB database and start the server at PORT
 db.once("open", () => {
     app.listen(PORT, () => {
         console.log(`Listening at ${PORT}`);
